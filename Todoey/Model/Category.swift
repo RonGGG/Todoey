63fbfe6b640b5adbc34fa4beafb3a355e5a5d712
Ron <ronguozirong@icloud.com>
2023-12-06 20:53:21 +1100

    Implement Functions with Realm

---
+++ Todoey/Model/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by 郭梓榕 on 6/12/2023.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by 郭梓榕 on 6/12/2023.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
